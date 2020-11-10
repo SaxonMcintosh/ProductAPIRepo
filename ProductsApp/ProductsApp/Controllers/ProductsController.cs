8998b9409b0dd339c3cd8f25e5062564f1212c02
Saxon McIntosh <saxon.mcintosh@gmail.com>
2020-11-10 09:28:10 -0500

    modifying products and solution config

--- ProductsApp/ProductsApp/Controllers/ProductsController.cs
+++ ProductsApp/ProductsApp/Controllers/ProductsController.cs
@@ -11,9 +11,9 @@ namespace ProductsApp.Controllers
     {
         Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 1, Name = "Pasketti and Yeetballs", Category = "Grockeries", Price = 100 },
+            new Product { Id = 2, Name = "Yo-yo Ma", Category = "Cellists", Price = 50 },
+            new Product { Id = 3, Name = "Mjolnir", Category = "Tangible Metaphors", Price = 80.75M }
         };
 
         public ProductsController() 
